c5c5f0d6460ffdc15cd9a3c0224f3d93b55c749f
LucasRoberto99 <lucasr.prof@gmail.com>
2023-10-31 11:54:56 +0100

    lucas

---
+++ src/components/Job.jsx
@@ -0,0 +1,13 @@
+const Job = (props) => {
+  console.log(props);
+  return (
+    <div className={props.className}>
+      <h2>{props.title}</h2>
+      <p>
+        {props.contractType} - {props.country} - {props.city}
+      </p>
+    </div>
+  );
+};
+
+export default Job;
---
+++ src/components/Jobs.jsx
@@ -0,0 +1,75 @@
+import Job from "./Job";
+
+const Jobs = () => {
+  return (
+    <>
+      <div className="jobs">
+        <Job
+          className="red"
+          title="Full Time Sales Associate - Sydney Boutique"
+          contractType="CDI"
+          country="Australie"
+          city="Sydney"
+        />
+        <Job
+          className="green"
+          title="Full Time Sales Associate - Sydney Boutique"
+          contractType="CDI"
+          country="Australie"
+          city="Sydney"
+        />
+        <Job
+          className="yellow"
+          title="Full Time Sales Associate - Sydney Boutique"
+          contractType="CDI"
+          country="Australie"
+          city="Sydney"
+        />
+        <Job
+          className="blue"
+          title="Full Time Sales Associate - Sydney Boutique"
+          contractType="CDI"
+          country="Australie"
+          city="Sydney"
+        />
+        <Job
+          className="pink"
+          title="Full Time Sales Associate - Sydney Boutique"
+          contractType="CDI"
+          country="Australie"
+          city="Sydney"
+        />
+        <Job
+          className="red"
+          title="Full Time Sales Associate - Sydney Boutique"
+          contractType="CDI"
+          country="Australie"
+          city="Sydney"
+        />
+        <Job
+          className="green"
+          title="Full Time Sales Associate - Sydney Boutique"
+          contractType="CDI"
+          country="Australie"
+          city="Sydney"
+        />
+        <Job
+          className="yellow"
+          title="Full Time Sales Associate - Sydney Boutique"
+          contractType="CDI"
+          country="Australie"
+          city="Sydney"
+        />
+        <Job
+          className="blue"
+          title="Full Time Sales Associate - Sydney Boutique"
+          contractType="CDI"
+          country="Australie"
+          city="Sydney"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Jobs;
